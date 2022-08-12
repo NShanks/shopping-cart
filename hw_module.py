c669281ff5e0d625da6401e97657c6e86cd6cf6c
Nicolas Shanks <nicolasshanks10@gmail.com>
2022-08-12 16:02:06 -0400

    homework part 2 commit

---
+++ hw_module.py
@@ -0,0 +1,11 @@
+from math import floor, pi
+print(floor(pi))
+
+def area(x, y):
+    area = (x * y)
+    return print(f"The area is {area} square units")
+
+
+def circumference(a):
+    circ = (2*pi*a)
+    return print(f"The circumference is {circ} units")
